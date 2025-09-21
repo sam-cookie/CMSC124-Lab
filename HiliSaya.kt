290debe26bdb91b279740e6010ff8135e7acc4b3
Sam Lodenn Lansoy <sdlansoy@up.edu.ph>
2025-09-21 16:29:13 +0800

    single token and multi-character operators added

--- HiliSaya.kt
+++ HiliSaya.kt
@@ -8,19 +8,90 @@ enum class TokenType {
     IDENTIFIER, NUMBER, STRING,
 
     // keywords
-    VAR, 
+    VAR,
 
+    // Comments
     EOF
 }
 
 data class Token(
     val type: TokenType,
     val lexeme: String,
-    val literal: Any? 
+    val literal: Any?, 
     val line: Int
 )
 
-class Scanner { 
-    print function 
-    shibal 
+
+fun scanToken(source: String, index: Int): Pair<TokenType?, Int> {
+    val c = source[index]
+    val next = if (index + 1 < source.length) source[index + 1] else '\u0000'
+
+    return if (c == '(') {
+        TokenType.LEFT_PAREN to 1
+    } else if (c == ')') {
+        TokenType.RIGHT_PAREN to 1
+    } else if (c == '{') {
+        TokenType.LEFT_BRACE to 1
+    } else if (c == '}') {
+        TokenType.RIGHT_BRACE to 1
+    } else if (c == '+') {
+        TokenType.PLUS to 1
+    } else if (c == '-') {
+        TokenType.MINUS to 1
+    } else if (c == '*') {
+        TokenType.TIMES to 1
+    } else if (c == '/') {
+        TokenType.DIVIDE to 1
+    } else if (c == '=') {
+        if (next == '=') TokenType.EQUALTO to 2
+        else TokenType.EQUALS to 1
+    } else if (c == ',') {
+        TokenType.COMMA to 1
+    } else if (c == ':') {
+        TokenType.COLON to 1
+    } else if (c == '<') {
+        if (next == '=') TokenType.LESS_THAN_EQUAL to 2
+        else TokenType.LESS_THAN to 1
+    } else if (c == '>') {
+        if (next == '=') TokenType.GREATER_THAN_EQUAL to 2
+        else TokenType.GREATER_THAN to 1
+    } else if (c == '"') { //di ko pa to naayos di ko gets? 
+        TokenType.DOUBLE_QUOTE to 1
+    } else {
+        null to 1 
+    }
+}
+
+fun main() {
+    val source = readLine() ?: ""   
+
+    val tokens = mutableListOf<Token>()
+    var i = 0
+    var line = 1
+
+    while (i < source.length) {
+        val c = source[i]
+
+        // whitespace handling 
+        if (c == ' ' || c == '\r' || c == '\t') {
+            i++
+            continue
+        }
+        if (c == '\n') {
+            line++
+            i++
+            continue
+        }
+
+        val (type, length) = scanToken(source, i)
+        if (type != null) {
+            val lexeme = source.substring(i, i + length)
+            tokens.add(Token(type, lexeme, null, line))
+        }
+        i += length
+    }
+
+    for (token in tokens) {
+        println(token)
+    }
 }
