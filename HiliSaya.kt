5f303ed9b33291de253875aeadef51937c749361
Sam Lodenn Lansoy <sdlansoy@up.edu.ph>
2025-09-30 02:38:34 +0800

    pasted readme, improved keyword and literals handling

--- HiliSaya.kt
+++ HiliSaya.kt
@@ -1,11 +1,12 @@
 enum class TokenType {
 
-    // single token
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, PLUS, MINUS, TIMES, DIVIDE, EQUALS,
-    COMMA, COLON, LESS_THAN_EQUAL, GREATER_THAN_EQUAL, LESS_THAN, GREATER_THAN, EQUALTO, NOT_EQUAL, DOUBLE_QUOTE,
+    // single token and multicharacter tokens 
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, PLUS, MINUS, TIMES, DIVIDE, MODULO, EQUALS,
+    COMMA, COLON, NOT, AND, OR, LESS_THAN_EQUAL, GREATER_THAN_EQUAL, LESS_THAN, GREATER_THAN, EQUALTO, NOT_EQUAL, DOUBLE_QUOTE,
+    ADD_ASSIGN, MINUS_ASSIGN, TIMES_ASSIGN, DIVIDED_ASSIGN, MODULO_ASSIGN
 
     // literals 
-    IDENTIFIER, NUMBER, STRING,
+    IDENTIFIER, NUMBER, STRING, 
 
     // keywords
     VAR, FUNCTION, RETURN, PRINT, IF, ELSE, WHILE, BREAK, CONTINUE, TRUE, FALSE, NULL,
@@ -46,31 +47,48 @@ fun scanToken(source: String, index: Int): Pair<TokenType?, Int> {
         ')' -> TokenType.RIGHT_PAREN to 1
         '{' -> TokenType.LEFT_BRACE to 1
         '}' -> TokenType.RIGHT_BRACE to 1
-        '+' -> TokenType.PLUS to 1
-        '-' -> TokenType.MINUS to 1
-        '*' -> TokenType.TIMES to 1
-        '/' -> TokenType.DIVIDE to 1
+        '+' -> if (next == '=') TokenType.ADD_ASSIGN to 2 else TokenType.PLUS to 1
+        '-' -> if (next == '=') TokenType.MINUS_ASSIGN to 2 else TokenType.MINUS to 1
+        '*' -> if (next == '=') TokenType.TIMES_ASSIGN to 2 else TokenType.TIMES to 1
+        '/' -> if (next == '=') TokenType.DIVIDED_ASSIGN to 2 else TokenType.DIVIDE to 1
+        '%' -> if (next == '=') TokenType.MODULO_ASSIGN to 2 else TokenType.MODULO to 1
         '=' -> if (next == '=') TokenType.EQUALTO to 2 else TokenType.EQUALS to 1
-        '!' -> if (next == '=') TokenType.NOT_EQUAL to 2  
+        '!' -> if (next == '=') TokenType.NOT_EQUAL to 2 else TokenType.NOT to 1
+        '&' -> if (next == '&') TokenType.AND to 2 else null to 1
+        '|' -> if (next == '|') TokenType.OR to 2 else null to 1
         ',' -> TokenType.COMMA to 1
         ':' -> TokenType.COLON to 1
         '<' -> if (next == '=') TokenType.LESS_THAN_EQUAL to 2 else TokenType.LESS_THAN to 1
         '>' -> if (next == '=') TokenType.GREATER_THAN_EQUAL to 2 else TokenType.GREATER_THAN to 1
-        '"' -> TokenType.DOUBLE_QUOTE to 1 // still placeholder for string handling
+        '"' -> TokenType.DOUBLE_QUOTE to 1
         else -> null to 1
     }
 }
 
 
+
 fun scanLiterals(source: String, index: Int): Pair<TokenType?, Int> {
     val c = source[index]
 
-    // number literal
+    // number literal (supports decimals)
     if (c.isDigit()) {
         var length = 1
-        while (index + length < source.length && source[index + length].isDigit()) {
-            length++
+        var hasDot = false
+
+        while (index + length < source.length) {
+            val ch = source[index + length]
+            if (ch.isDigit()) {
+                length++
+            } else if (ch == '.' && !hasDot && index + length + 1 < source.length && source[index + length + 1].isDigit()) {
+                hasDot = true
+                length++
+            } else {
+                break
+            }
         }
+
+        val lexeme = source.substring(index, index + length)
+        val value = lexeme.toDoubleOrNull()
         return TokenType.NUMBER to length
     }
 
@@ -85,8 +103,6 @@ fun scanLiterals(source: String, index: Int): Pair<TokenType?, Int> {
         val lexeme = source.substring(index, index + length)
         val type = keywords[lexeme] ?: TokenType.IDENTIFIER
         return type to length
-    } else {
-        println(error)
     }
 
     // string literal
@@ -97,6 +113,8 @@ fun scanLiterals(source: String, index: Int): Pair<TokenType?, Int> {
         }
         return if (index + length < source.length) {
             length++ // include closing "
+            val lexeme = source.substring(index, index + length)
+            val literalValue = lexeme.substring(1, lexeme.length - 1) // strip quotes
             TokenType.STRING to length
         } else {
             // unterminated string
@@ -130,24 +148,24 @@ fun main() {
 
         // block comments
         if (i + 2 < source.length && source.substring(i, i + 3) == "///") {
-            val closing = source.indexOf("///", i + 3) 
+            val closing = source.indexOf("///", i + 3)
             val endComment = if (closing != -1) closing + 3 else source.length
 
-            val lexeme = source.substring (i, endComment)
+            val lexeme = source.substring(i, endComment)
 
             // count new lines
-            line += lexeme.count {it == '\n'}
+            line += lexeme.count { it == '\n' }
 
             i = endComment
             continue
         }
 
-         // line comments
+        // line comments
         if (i + 1 < source.length && source.substring(i, i + 2) == "//") {
-            val closing = source.indexOf("//", i + 2) 
+            val closing = source.indexOf("//", i + 2)
             val endComment = if (closing != -1) closing + 2 else source.length
 
-            val lexeme = source.substring (i, endComment)
+            val lexeme = source.substring(i, endComment)
 
             i = endComment
             continue
@@ -156,7 +174,14 @@ fun main() {
         val (litType, litLen) = scanLiterals(source, i)
         if (litType != null) {
             val lexeme = source.substring(i, i + litLen)
-            tokens.add(Token(litType, lexeme, null, line))
+
+            val literal = when (litType) {
+                TokenType.NUMBER -> lexeme.toDoubleOrNull()
+                TokenType.STRING -> lexeme.substring(1, lexeme.length - 1) // remove quotes
+                else -> null
+            }
+
+            tokens.add(Token(litType, lexeme, literal, line))
             i += litLen
             continue
         }
@@ -171,8 +196,7 @@ fun main() {
 
     tokens.add(Token(TokenType.EOF, "", null, line))
 
-    // print results
     for (token in tokens) {
-        println(token)
+        println("Token(type=${token.type}, lexeme=${token.lexeme}, literal=${token.literal}, line=${token.line})")
     }
 }
